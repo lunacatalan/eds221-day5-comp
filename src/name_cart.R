c3f83a2ae0fa629ce4794250677514d311462320
Luna Catalan <lunahcatalan@gmail.com>
2023-08-18 14:00:13 -0700

    add r-script

---
+++ src/name_cart.R
@@ -0,0 +1,13 @@
+#' Make food cart name
+#'
+#' @param food your favorite food
+#' @param animal your favorite animal
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+name_cart <- function(food, animal) {
+  paste0("Mc", stringr::str_to_title(animal), "'s ", stringr::str_to_title(food), "Mart")
+}
